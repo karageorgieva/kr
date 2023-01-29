f1fbb967d2b98c64f87ca873b9de6779e77d0e07
Darya <karageorgieva@icloud.com>
2023-01-29 23:25:46 +0300

    finish

--- FINSLVHECK/Program.cs
+++ FINSLVHECK/Program.cs
@@ -4,9 +4,34 @@ Clear();
 
 string[] array = EnteredArray();
 
+string[] result = FindLessThree(array, 3);
 
+WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");
 
 string[] EnteredArray() {
     Write("Введите строки через пробел: ");
     return ReadLine().Split(" ");
+}
+string[] FindLessThree(string[] input, int n) {
+    string[] output = new string[CountLessThree(input, n)];
+
+    for(int i = 0, a = 0; i < input.Length; i++) {
+        if(input[i].Length <= n) {
+            output[a] = input[i];
+            a++;
+        }
+    }
+
+    return output;
+}
+int CountLessThree(string[] input, int n) {
+    int count = 0;
+
+    for(int i = 0; i < input.Length; i++) {
+        if(input[i].Length <= n) {
+            count++;
+        }
+    }
+
+    return count;
 }
